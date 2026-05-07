1168e1188d84c79de43fc2083cbb13f3c5e5438d
sumit awinash <52064599+sumitawinash@users.noreply.github.com>
2026-05-07 13:00:36 +0530

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const webpack = require('webpack');
+
+
+
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+
+module.exports = {
+    entry: [ "./src/index.ts", './src/worker.ts'],
+    output: {
+        libraryExport: "default",
+        path: path.resolve(__dirname, './dist'),
+        filename: "main.js"
+    },
+    module: {
+
+        rules: [
+            {
+                test: /\.(ts|js)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "babel-loader"
+                    },
+                    {
+                        loader: "ts-loader",
+                        options: {
+                            allowTsInNodeModules: false
+                        }
+                    }
+                ],
+            },
+
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader", "postcss-loader"],
+            }
+
+        ],
+
+    },
+
+    plugins: [
+
+        new HtmlWebpackPlugin({
+            template: 'src/index.html'
+        }),
+
+        new CleanWebpackPlugin({
+            cleanStaleWebpackAssets: false
+        }),
+        new CopyWebpackPlugin( {
+            patterns: [
+                { from: "src/*.js", to: path.basename('[name].js') },
+                { from: "src/img/*.svg", to: path.basename('[name].svg') },
+                { from: "src/img/*.png", to: path.basename('[name].png') },
+
+            ]
+        })
+
+    ],
+    resolve: {
+        extensions: [".ts", ".tsx", ".js",  ".css"]
+    },
+
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'dist'),
+        },
+        compress: true,
+        port: 8080,
+        allowedHosts: "all",
+    },
+
+    mode: 'development'
+
+};
